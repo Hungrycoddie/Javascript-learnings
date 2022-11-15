0f7de0acbf875310cc427dc6e4bb0d10def35705
Ar <hungrycodie@gmail.com>
2022-11-15 17:54:16 +0530

    Access Array Data with Indexes

--- Learning.js
+++ Learning.js
@@ -134,3 +134,18 @@ const myArray_a1 = [
   ["Aruna", 01],
 ];
 console.log(myArray_a1)
+
+
+/*
+               Access Array Data with Indexes
+We can access the data inside arrays using indexes.
+
+Array indexes are written in the same bracket notation that strings use, 
+except that instead of specifying a character, they are specifying an entry in
+the array. Like strings, arrays use zero-based indexing, so the first element 
+in an array has an index of 0.
+*/
+const myArray_a2 = [50, 60, 70];
+var myData = myArray_a2[0];
+console.log(myData);
+
